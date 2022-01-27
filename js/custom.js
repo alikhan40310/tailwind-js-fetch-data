81f063590c1683d0e5f6e4e014fc2609b41cbd3a
Ali Khan <alikhan40310@gmail.com>
2022-01-27 19:43:02 +0500

    other changes

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@ fetch('https://jsonplaceholder.typicode.com/photos').then(function(data){
   return data.json()
 }).then(function(data){
   // using for each loop
-  const sliceArray = data.slice(0,5);
+  const sliceArray = data.slice(0,50);
   sliceArray.forEach(function(a){
     // creating elements of required data
     let mainDiv = document.createElement("div");
@@ -16,6 +16,10 @@ fetch('https://jsonplaceholder.typicode.com/photos').then(function(data){
     // replacing element with innerHtml
     title.innerHTML = a.title;
     image.src = a.url;
+    // styling of apend element
+    mainDiv.style.width ="300px";
+    title.style.fontSize="18px";
+    title.style.marginTop="10px";
 
     document.getElementById('mainSection').appendChild(mainDiv);
   });
