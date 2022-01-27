aa0fa0ea7f2cbb04b036690ab9231ad01fadc0d9
Ali Khan <alikhan40310@gmail.com>
2022-01-27 19:32:48 +0500

    new changes

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,25 @@
+
 // fatching data
+fetch('https://jsonplaceholder.typicode.com/photos').then(function(data){
+  return data.json()
+}).then(function(data){
+  // using for each loop
+  const sliceArray = data.slice(0,5);
+  sliceArray.forEach(function(a){
+    // creating elements of required data
+    let mainDiv = document.createElement("div");
+    let title = document.createElement("p");
+    let image = document.createElement("img");
+    // append data in main div
+    mainDiv.appendChild(image);
+    mainDiv.appendChild(title);
+    // replacing element with innerHtml
+    title.innerHTML = a.title;
+    image.src = a.url;
+
+    document.getElementById('mainSection').appendChild(mainDiv);
+  });
+})
+
+
+
